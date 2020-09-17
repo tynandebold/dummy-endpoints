1e45b307cd2dbf5e1707e0a28893787a91d59809
Tynan DeBold <thdebold@gmail.com>
2020-09-17 11:26:19 +0200

    update port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 9001;
+const port = process.env.PORT || 9001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
